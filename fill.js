fd3bc4fe3574fb46c4e60c4d34a6bf441404a0ff
aspucollege <148790443+aspucollege@users.noreply.github.com>
2023-10-25 20:14:42 +0400

    Add files via upload

---
+++ fill.js
@@ -0,0 +1,25 @@
+const return_btn = document.querySelector(".return-btn");
+const submit_btn = document.querySelector(".submit-btn");
+const scriptURL = "https://script.google.com/macros/s/AKfycbyWEL99eTisajfB_zkX9mBQ952_GoxSboetH0Tz4oQfJjbtvi6jGC9iTfndi2GSNeCQfA/exec"
+const form = document.forms['contactform']
+
+
+return_btn.addEventListener("click", () => {
+	window.open("index.html");
+});		
+
+form.addEventListener('submit', e => {
+	var name_value = document.querySelector(".name-value").value;
+	var surname_value = document.querySelector(".surname-value").value;
+	var faculty_value = document.querySelector(".faculty-value").value;
+	var class_value = document.querySelector(".class-value").value;
+	var social_media_value = document.querySelector(".social-media-value").value;
+	var contact_value = document.querySelector(".contact-value").value;
+	var event_value = document.querySelector(".event-value").value;
+
+	e.preventDefault()
+	fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+		.then(response => alert("Հարգելի " + name_value + " " + surname_value + " (" + faculty_value + " " + class_value + "), Ձեր հայտը հաջողությամբ ուղարկվել է: " + event_value + " մրցույթին մասնակցելու համար Ձեզ հետ կապ կհաստատեն հետևյալ տվյալներով - " + social_media_value + " - " + contact_value), form.reset())
+		.catch(error => alert("Ինչ-որ բան այն չէ: Խնդրում ենք փորձել նորից:"))
+		    
+});
